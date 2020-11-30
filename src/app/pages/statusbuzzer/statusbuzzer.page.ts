393f54eed138d10acc9d4deeb50e75d34d6f3656
gircost <gircost@gmail.com>
2020-11-30 00:10:23 -0300

    fourth commit

--- src/app/pages/statusbuzzer/statusbuzzer.page.ts
+++ src/app/pages/statusbuzzer/statusbuzzer.page.ts
@@ -1,15 +1,82 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DweetService } from 'src/app/services/dweet.service'; 
+import Dweet from 'src/models/Dweet';
 
 @Component({
-  selector: 'app-status-buzzer',
-  templateUrl: './status-buzzer.page.html',
-  styleUrls: ['./status-buzzer.page.scss'],
+  selector: 'app-statusbuzzer',
+  templateUrl: './statusbuzzer.page.html',
+  styleUrls: ['./statusbuzzer.page.scss'],
 })
-export class StatusBuzzerPage implements OnInit {
+export class StatusbuzzerPage implements OnInit {
 
-  constructor() { }
+  private dweet: Dweet 
+  private isLoading: boolean = true; 
+  private time: any; 
+  private dataPlot: Array<any>
+  options: Object;
+  
+  constructor(private dweetService: DweetService, public router: Router) { 
+    this.time = setInterval(() => {this.getLastDweets()}, 3000)
+  }
+
+  private getLastDweets(){ 
+    this.dataPlot = [] 
+    this.dweetService.loadLastDweets().subscribe( 
+      data => { 
+        this.preencherDweet(data) 
+      }, 
+      err => { 
+        console.log("Erro: ", err) 
+      }, 
+      () => this.isLoading = false 
+    ) 
+  }
+
+  private preencherDweet(data: any){ 
+    this.dweet = this.dweetService.preencherDweet(data); 
+    this.loadDataForPlot(this.dweet) 
+    this.plotChart(); 
+  }
+
+  private loadDataForPlot(dweet: Dweet){ 
+    for(let _with of dweet.with){ 
+      let epoch = new Date(_with.created).getTime() 
+      this.dataPlot.push([epoch, _with.content.$statusbuzzer]) 
+    } 
+  }
+
+  private plotChart(){ 
+    this.options = { 
+      xAxis: {
+        type: 'datetime' 
+      }, 
+      yAxis: { 
+        labels: { 
+          formatter: function(){ 
+            return this.value; 
+          } 
+        }, 
+      }, 
+      title: { text: 'Statusbuzzer '}, 
+        series: [{ 
+          name: 'statusbuzzer', 
+          data: this.dataPlot.reverse(), 
+          pointInterval: 60 * 60 
+        }] 
+      }; 
+    }
+
+  goBack(){
+    this.router.navigate(['home'])
+  }
 
   ngOnInit() {
+    this.getLastDweets();
+  }
+
+  ngOnDestroy(){ 
+    clearInterval(this.time) 
   }
 
 }
